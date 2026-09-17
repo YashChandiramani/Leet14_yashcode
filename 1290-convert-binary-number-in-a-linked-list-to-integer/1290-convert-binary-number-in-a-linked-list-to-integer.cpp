91859063763ce1c1024b5e16090a3ab623e7b6bd
YashChandiramani <chandiramaniyash@gmail.com>
2026-09-17 23:55:00 +0530

    Time: 0 ms (100%), Space: 10.5 MB (72.77%) - LeetHub

--- 1290-convert-binary-number-in-a-linked-list-to-integer/1290-convert-binary-number-in-a-linked-list-to-integer.cpp
+++ 1290-convert-binary-number-in-a-linked-list-to-integer/1290-convert-binary-number-in-a-linked-list-to-integer.cpp
@@ -11,30 +11,12 @@
 class Solution {
 public:
     int getDecimalValue(ListNode* head) {
-        vector<int> bin;
-        vector<int> multi;
-        vector<int> ans;
         long long num = 0;
         ListNode* curr = head;
         while(curr != nullptr){
-            bin.push_back(curr->val);
+            int curr_bit = curr->val;
+            num = num * 2 + curr_bit;
             curr = curr->next;
-        }
-        int org = bin.size();
-        int n = org - 1;
-        while(n >= 0){
-            multi.push_back(pow(2, n));
-            if(n == 0){
-                multi.push_back(1);
-            }
-            n--;
-        }
-        for(int i = 0; i < org; ++i){
-            long long push = bin[i] * multi[i];
-            ans.push_back(push);
-        }
-        for(int i = 0; i < ans.size(); ++i){
-            num = num + ans[i];
         }
         return num;
     }
